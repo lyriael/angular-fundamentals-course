a61ccf2dc1bc7f9efb2cbb6bde3a6a51132863d8
Judith Fuog <lyriael@gmx.net>
2020-05-08 10:22:39 +0200

    NG-Fundamentals: 11. Interpolation and expressions

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core'
+import {Component} from '@angular/core'
 
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
-  <div class="app">
-    {{ title }}
-  </div>
+    <div class="app">
+      {{ title }}
+      <div>
+        {{ !isHappy ? ":)" : ":(" }}
+      </div>
+    </div>
   `
 })
 export class AppComponent {
   title: string;
+  numberOne: number = 1;
+  numberTwo: number = 2;
+  isHappy: boolean = true;
 
   constructor() {
     this.title = 'Ultimate Angular';
