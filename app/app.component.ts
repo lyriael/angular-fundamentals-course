322e6ac0400e84987a6955f93fabbd2a6297fe1d
Judith Fuog <lyriael@gmx.net>
2020-05-08 11:33:56 +0200

    NG-Fundamentals: 16. ngif, * syntax and <ng-template>

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,18 +5,28 @@ import {Component} from '@angular/core'
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      <button (click)=handleClick(username.value)>
-        Reset Name
-      </button>
-      <input type="text" #username>
-      <div>{{ name }}</div>
+      <input
+        type="text"
+        [value]="name"
+        (input)="handleChange($event)">
+
+      <div *ngIf="name.length > 2">
+        Searching for... {{ name }}
+      </div>
     </div>
+
+    <!-- * is just shorthand, so that we don't have to write template and bind ngIf-->
+    <template [ngIf]="name.length > 2">
+      <div>
+        Searching for... {{name}}
+      </div>
+    </template>
   `
 })
 export class AppComponent {
-  name: string = "Yi";
+  name: string = '';
 
-  handleClick(value: string) {
-    console.log(value);
+  handleChange(event: any) {
+    this.name = event.target.value;
   }
 }
