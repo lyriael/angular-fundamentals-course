ba741c79591285f663609ec245abb7bca42b8181
Judith Fuog <lyriael@gmx.net>
2020-05-11 11:56:53 +0200

    NG-Fundamentals: 18. ngClass and className bindings

--- app/app.component.ts
+++ app/app.component.ts
@@ -13,13 +13,21 @@ interface Passenger {
     <div class="app">
       <h3>Airline Passengers</h3>
       <ul>
-        <template ngFor let-passenger let-i="index" [ngForOf]="passengers">
-          {{ i }}: {{ passenger.fullname }}
-        </template>
+        <li *ngFor="let passenger of passengers; let i = index">
+          <span
+            class="status"
+            [ngClass]="{
+            'checked-in': passenger.checkedIn,
+            'checked-out': !passenger.checkedIn
+            }"></span>
+            {{ i }}: {{ passenger.fullname }}
+        </li>
       </ul>
-      <h3>Airline Passengers</h3>
       <ul>
         <li *ngFor="let passenger of passengers; let i = index">
+          <span
+            class="status"
+            [class.checked-in]="passenger.checkedIn"></span>
             {{ i }}: {{ passenger.fullname }}
         </li>
       </ul>
