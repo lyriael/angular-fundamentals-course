8577590fbd10141e61a4af7365400a9a874628fd
Judith Fuog <lyriael@gmx.net>
2020-05-20 17:43:09 +0200

    NG-Fundamentals: 25. Creating a container (smart) component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,76 +1,13 @@
 import {Component} from '@angular/core'
 
-interface Child {
-  name: string,
-  age: number,
-}
-
-interface Passenger {
-  id: number,
-  fullname: string,
-  checkedIn: boolean,
-  checkInDate: number | null,
-  children: Array<Child> | null,
-}
-
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      <h3> Airline Passengers</h3>
-      <ul>
-        <li *ngFor="let passenger of passengers; let i = index">
-          <span
-            class="status"
-            [class.checked-in]="passenger.checkedIn"></span>
-          {{ i }}: {{ passenger.fullname}}
-          <p> {{ passenger | json }}</p>
-          <div class="date">
-            Check in date:
-            {{ passenger.checkInDate ? (passenger.checkInDate | date: 'y MMMM d'| uppercase) : 'Not checked in' }}
-          </div>
-          <div class="children">
-            Children: {{ passenger.children?.length || 0 }}
-          </div>
-        </li>
-      </ul>
-
+      <passenger-dashboard></passenger-dashboard>
     </div>
   `
 })
 export class AppComponent {
-  passengers: Passenger[] = [
-    {
-      id: 1,
-      fullname: 'Stephen',
-      checkedIn: true,
-      checkInDate: 1490742000000,
-      children: null,
-    }, {
-      id: 2,
-      fullname: 'Rose',
-      checkedIn: false,
-      checkInDate: null,
-      children: [{name: 'Ted', age: 12}, {name: 'Mug', age: 17}],
-    }, {
-      id: 3,
-      fullname: 'James',
-      checkedIn: true,
-      checkInDate: 1490742000000,
-      children: null,
-    }, {
-      id: 4,
-      fullname: 'Louise',
-      checkedIn: true,
-      checkInDate: 1490742000000,
-      children: [{name: 'Funa', age: 12}, {name: 'Marc', age: 17}],
-    }, {
-      id: 5,
-      fullname: 'Caith',
-      checkedIn: false,
-      checkInDate: null,
-      children: null,
-    },
-  ]
 }
