1e5bfd4a47a5eed8b76f0ec9498728da414a9c0a
Judith Fuog <lyriael@gmx.net>
2020-05-20 17:25:31 +0200

    NG-Fundamentals: 24. Feature modules with @NgModule

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,13 +2,20 @@ import {AppComponent} from "./app.component";
 import {BrowserModule} from "@angular/platform-browser";
 import {CommonModule} from "@angular/common";
 
+import {PassengerDashboardModule} from "./passenger-dashboard/passenger-dashboard.module";
+
 import {NgModule} from "@angular/core";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [BrowserModule, CommonModule],
+  imports: [
+    // Angular Modules
+    BrowserModule, CommonModule,
+    // Custom Modules
+    PassengerDashboardModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
