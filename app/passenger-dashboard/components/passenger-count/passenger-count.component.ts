dc5538c9f5350ff0345713b846447f9e14d18ef1
Judith Fuog <lyriael@gmx.net>
2020-06-10 14:29:49 +0200

    NG-Fundamentals: 27. Presentational (dumb) components

---
+++ app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
@@ -0,0 +1,14 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'passenger-count',
+  template: `
+  <div>
+
+  </div>
+  `
+})
+export class PassengerCountComponent {
+  constructor() {
+  }
+}
--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -1,11 +1,18 @@
 import {CommonModule} from "@angular/common";
 import {NgModule} from "@angular/core";
 
+// containers
 import {PassengerDashboardComponent} from "./containers/passenger-dashboard/passenger-dashboard.component";
+
+// components
+import {PassengerCountComponent} from "./components/passenger-count/passenger-count.component";
+import {PassengerDetailComponent} from "./components/passenger-detail/passenger-detail.component";
 
 @NgModule({
   declarations: [
     PassengerDashboardComponent,
+    PassengerCountComponent,
+    PassengerDetailComponent,
   ],
   imports: [
     CommonModule
