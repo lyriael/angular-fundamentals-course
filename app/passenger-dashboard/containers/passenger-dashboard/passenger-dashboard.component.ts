d42d24616999511c1a48072a1a9d7a22317d9a46
Judith Fuog <lyriael@gmx.net>
2020-06-11 09:20:16 +0200

    NG-Fundamentals: 31. Immutable state changes

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -59,10 +59,19 @@ export class PassengerDashboardComponent implements OnInit {
       },
     ]
   }
-  handleEdit(event) {
-    console.log(event);
+
+  handleEdit(event: Passenger) {
+    this.passengers = this.passengers.map((passenger: Passenger) => {
+      if (passenger.id === event.id) {
+        passenger = Object.assign({}, passenger, event); // object assign merges
+      }
+      return passenger;
+    });
   }
-  handleRemove(event) {
-    console.log(event);
+
+  handleRemove(event: Passenger) {
+    this.passengers = this.passengers.filter((passenger: Passenger) => {
+      return passenger.id !== event.id;
+    });
   }
 }
