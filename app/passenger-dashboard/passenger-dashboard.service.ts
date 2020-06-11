05516c480101e9d017ea37a6f098a9f014e6f2b8
Judith Fuog <lyriael@gmx.net>
2020-06-11 10:58:33 +0200

    NG-Fundamentals: 35. Http data fetching with Observables

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -1,45 +1,20 @@
 import {Passenger} from "./models/passenger.interface";
-import {Http} from "@angular/http";
+import {Http, Response} from "@angular/http";
 import {Injectable} from "@angular/core";
+import {Observable} from "rxjs/Observable";
+import 'rxjs/add/operator/map'
+const PASSENGER_API: string = '/api/passengers';
 
 @Injectable()
 export class PassengerDashboardService {
-  constructor(private http: Http) {}
 
-  getPassengers(): Passenger[] {
-    return [
-      {
-        id: 1,
-        fullname: 'Stephen',
-        checkedIn: true,
-        checkInDate: 1490742000000,
-        children: null,
-      }, {
-        id: 2,
-        fullname:  'Rose',
-        checkedIn: false,
-        checkInDate: null,
-        children: [{name: 'Ted', age: 12}, {name: 'Mug', age: 17}],
-      }, {
-        id: 3,
-        fullname: 'James',
-        checkedIn: true,
-        checkInDate: 1490742000000,
-        children: null,
-      }, {
-        id: 4,
-        fullname: 'Louise',
-        checkedIn: true,
-        checkInDate: 1490742000000,
-        children: [{name: 'Funa', age: 12}, {name: 'Marc', age: 17}],
-      }, {
-        id: 5,
-        fullname: 'Caith',
-        checkedIn: false,
-        checkInDate: null,
-        children: null,
-      },
-    ]
+  constructor(private http: Http) {
+  }
+
+  getPassengers(): Observable<Passenger[]> {
+    return this.http
+      .get(PASSENGER_API)
+      .map((response: Response) => response.json());
   }
 
 }
