80f9d9c4a3e665e2ea66eafded3c7aa443e218ab
Judith Fuog <lyriael@gmx.net>
2020-06-11 09:51:31 +0200

    NG-Fundamentals: 34. Understanding @Injectable

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -1,8 +1,10 @@
 import {Passenger} from "./models/passenger.interface";
+import {Http} from "@angular/http";
+import {Injectable} from "@angular/core";
 
+@Injectable()
 export class PassengerDashboardService {
-  constructor() {
-  }
+  constructor(private http: Http) {}
 
   getPassengers(): Passenger[] {
     return [
