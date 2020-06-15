0c7f83b177cb1916f080780a8a8e179f1d36b9d3
Judith Fuog <lyriael@gmx.net>
2020-06-15 10:01:44 +0200

    NG-Fundamentals: 40. Forms container component

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -20,6 +20,13 @@ export class PassengerDashboardService {
       .catch((error: any) => Observable.throw(error.json));
   }
 
+  getPassenger(id: number): Observable<Passenger> {
+    return this.http
+      .get(`${PASSENGER_API}/${id}`)
+      .map((response: Response) => response.json())
+      .catch((error: any) => Observable.throw(error.json));
+  }
+
   updatePassenger(passenger: Passenger): Observable<Passenger> {
     let options = new RequestOptions({
       headers: new Headers({
